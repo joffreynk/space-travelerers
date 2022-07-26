f87dfab16d43dba578438bd2b0ec97f82e809681
Joffrey <joyjoffrey@gmail.com>
2022-07-26 22:38:54 +0200

    fetch missions

--- src/redux/missions/missionsActionCreator.js
+++ src/redux/missions/missionsActionCreator.js
@@ -1,1 +1,22 @@
-// import { GET_DATA, JOIN_MISSION, LEAVE_MISSION } from './missionsActions';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { GET_DATA, JOIN_MISSION, LEAVE_MISSION } from './missionsActions';
+
+export const getData = createAsyncThunk(GET_DATA, async () => {
+  const res = await fetch('https://api.spacexdata.com/v3/missions');
+  const missions = await res.json();
+  const missionsData = [];
+  missions.forEach((mission) => {
+    missionsData.push({
+      id: mission.mission_id,
+      mission: mission.mission_name,
+      description: mission.description,
+      reserved: false,
+    });
+  });
+  return missionsData;
+});
+
+export const joinMission = (id) => ({ action: JOIN_MISSION, payload: id });
+
+export const leaveMission = (id) => ({ action: LEAVE_MISSION, payload: id });
