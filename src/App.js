1028d487b36532ca25bbabca57faf440e8f59826
judah <87juda@gmail.com>
2022-07-27 18:46:19 -0300

    Update linter settings

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Navbar from './components/Header';
 import Missions from './components/Missions';
 import Rockets from './components/Rockets';
 import DragonsContainer from './components/DragonsContainer';
-
-import './App.css';
 import MyProfile from './components/MyProfile';
 
 function App() {
