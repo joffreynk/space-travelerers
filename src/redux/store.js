21090176aa902868e4c3cc884d706e75da593316
Joffrey <joyjoffrey@gmail.com>
2022-07-28 00:35:43 +0200

    fix linters

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,11 @@
-
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import missionsSlice from './missions/missionsReducer';
 
-const reducer = combineReducers(
-  {
+const reducer = combineReducers({
     missions: missionsSlice.reducer,
   },
 );
 
 const store = configureStore({ reducer });
-
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
-
 
 export default store;
