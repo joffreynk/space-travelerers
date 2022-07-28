68a742605584961bdad811db44ed2da73aad4fcb
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-07-28 15:07:58 -0600

    added testing

---
+++ src/components/__test__/dragon.test.js
@@ -0,0 +1,35 @@
+// Dragon.test.js
+
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import DragonsContainer from '../DragonsContainer';
+import store from '../../redux/store';
+
+describe('Dragon Component Rendering ', () => {
+
+  it('Matches snapshot', () => {
+    const Tree = renderer.create(
+      <MemoryRouter>
+        <Provider store={store}>
+          <DragonsContainer />
+        </Provider>
+      </MemoryRouter>,
+    ).toJSON();
+    expect(Tree).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <DragonsContainer />
+        </Provider>,
+      );
+    expect(tree).toMatchSnapshot();
+  });
+  
+});
+
+ 
+
